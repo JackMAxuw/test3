e8d346d4626a1ae2202dbbb521606a778adde5e5
jyp.1129 <jackie1129008@gmail.com>
2020-12-05 10:35:01 +0800

    1.3

---
+++ 1.3.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int n, x = 1, y;
+
+	cout << "Enter n\n";
+	cin >> n;
+	if (n >= 2)
+		cout << "2 ";
+	for (y = 2; y <= n; y++) {
+		for (x = 2; x <= y; x++) {
+			if (y % x == 0)
+				break;
+			else if (y == x + 1)
+				cout << y << " ";
+			}
+	}
+}
